ceecd8618062cc89c7510de042736efa115419bf
newham <newham.cn@gmail.com>
2019-12-19 17:54:10 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+var electron = require('electron');
+const { app, Menu } = require('electron')
+var BrowserWindow = electron.BrowserWindow;
+var mainWindow = null;
+
+function buildMenu() {
+    if (process.platform === 'darwin') {
+        const template = [
+            {
+                label: "Application",
+                submenu: [
+                    { label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+                ]
+            },
+            {
+                label: "Edit",
+                submenu: [
+                    { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                    { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                ]
+            }
+        ];
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    } else {
+        Menu.setApplicationMenu(null)
+    }
+}
+
+function openWindow(){
+    buildMenu();
+    openMain();
+}
+
+function openMain() {
+    if (mainWindow == null) {
+        //设置窗口大小等参数
+        mainWindow = new BrowserWindow({ width: 520, height: 730, minWidth: 520, minHeight: 730, resizable: false })
+        //首页
+        mainWindow.loadFile('format.html');
+        //注册关闭事件
+        mainWindow.on('closed', () => {
+            mainWindow = null;
+        })
+    } 
+}
+
+app.on('ready', function () {
+    openWindow();
+})
+
+app.on('activate', function () {
+    openWindow();
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+// 关闭所有窗口后退出程序
+// app.on('window-all-closed', () => {
+//     app.quit()
+// })
